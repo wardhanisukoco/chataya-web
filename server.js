fe89505ee2942026f51af66e5856575f3894fcb4
Wardhani Sukoco <wardhanisukoco@gmail.com>
2022-10-25 16:19:13 +0700

    fix: force tls

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const
 
 const app = express()
 
-app.use(function (request, response, next) {
-  if (process.env.NODE_ENV != 'development' && !request.secure) {
-    return response.redirect("https://" + request.headers.host + request.url);
-  }
-  next();
-})
+// app.use(function (request, response, next) {
+//   if (process.env.NODE_ENV != 'development' && !request.secure) {
+//     return response.redirect("https://" + request.headers.host + request.url);
+//   }
+//   next();
+// })
 
 app.use(history())
 app.use(serveStatic(__dirname + '/dist/spa'))
